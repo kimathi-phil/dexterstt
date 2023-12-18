7b5478d3c48a04d6731d62f3ae9b09be9b974782
Philip Kimathi <kimathi.phil@gmail.com>
2023-12-18 12:40:22 +0300

    feat: implement send audio usecase

---
+++ lib/features/transcriptions/domain/usecases/send_audio_get_transcriptions_usecase.dart
@@ -0,0 +1,30 @@
+import 'package:dartz/dartz.dart';
+
+import '../../../../core/usecases/usecases.dart';
+import '../entities/transcription.dart';
+import '../repositories/transcriptions_repository.dart';
+import 'package:injectable/injectable.dart';
+
+@lazySingleton
+class SendAudioGetTranscriptsUseCase
+    implements UseCaseTwoResults<Transcripts, String> {
+  SendAudioGetTranscriptsUseCase(this._transcriptsRepository);
+
+  final TranscriptsRepository _transcriptsRepository;
+
+  @override
+  Future<Either<String, Transcripts>> call({required String params}) async {
+    final eitherResult =
+        await _transcriptsRepository.sendAudioGetTranscript(filePath: params);
+
+    return eitherResult.fold(
+      (exception) =>
+          Left(_mapExceptionToString(exception)),
+      (transcript) => Right(transcript),
+    );
+  }
+
+  String _mapExceptionToString(Exception e) {
+    return e.toString();
+  }
+}
