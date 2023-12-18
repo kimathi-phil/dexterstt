4ffa31141c1699495c5a4ce4daf94af14e541153
Philip Kimathi <kimathi.phil@gmail.com>
2023-12-18 12:42:06 +0300

    feat: add transcription states

---
+++ lib/features/transcriptions/application/transcriptions/transcriptions_cubit.dart
@@ -0,0 +1,8 @@
+import 'dart:async';
+import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:injectable/injectable.dart';
+import '../../domain/entities/transcription.dart';
+import '../../domain/usecases/usecases.dart';
+part 'transcriptions_state.dart';
